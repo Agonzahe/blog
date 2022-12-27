649216962b5ff374c111d6649f7575fdaaa3d165
Agonzahe <agonzahe20@gmail.com>
2022-12-27 12:42:24 -0300

    ResueltoAP3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,23 @@ class PagesController < ApplicationController
 
   def contacto
   end
+
+  def articulo
+    @articulo = Articulo.all
+  end
+  
+  def new
+    @articulo = Articulo.new
+  end
+  
+  def create
+    data_articulo = params.require(:articulo).permit(:titulo, :contenido)
+    @articulo = Articulo.new(data_articulo)
+    if @articulo.save
+        redirect_to articulo_path
+    else
+        render :new
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'pages#index'
   get '/contacto', to: 'pages#contacto'
+  get '/articulo', to: 'pages#articulo'
+  get '/articulo/new', to: 'pages#new'
+  post '/articulo', to: 'pages#create'
 end
